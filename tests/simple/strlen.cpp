ea1a12edd87cb7e3de5f3bfff0ce8c23e43f1ff3
Guillaume DEPARDON <gudepard@e2r11p8.42.fr>
2015-01-21 19:15:47 +0100

    Refactored strlen tests. Kept the betonicity intact

--- tests/simple/strlen.cpp
+++ tests/simple/strlen.cpp
@@ -6,17 +6,15 @@ extern "C" {
     size_t ft_strlen(const char *);
 }
 
+static void test_one(const char * str)
+{
+    auto strlen_ret = ::strlen(str); // System call
+    auto ft_strlen_ret = ::ft_strlen(str); // Betonic call
+    assert(strlen_ret == ft_strlen_ret);
+}
+
 void test_strlen()
 {
-    size_t  systemCall;
-    size_t  betonicCall;
-    char    str[] = "Hello World";
-
-    systemCall = strlen(str);
-    betonicCall = ft_strlen(str);
-
-    std::cout << systemCall << std::endl;
-    std::cout << betonicCall << std::endl;
-
-    assert(systemCall == betonicCall);
+    test_one("Hello World");
+    test_one("");
 }
