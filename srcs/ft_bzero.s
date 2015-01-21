1b9047e4c7757fe48054aa90e3e04a34a086c20a
Oscar CHASE <ochase@e2r11p10.42.fr>
2015-01-21 12:54:20 +0100

    fuck you again

--- srcs/ft_bzero.s
+++ srcs/ft_bzero.s
@@ -2,7 +2,6 @@ section .text
 global _ft_bzero
 
 _ft_bzero:
-<<<<<<< HEAD
 	test	rsi, rsi
 	je		ret1
 	mov		rcx, rsi
@@ -10,16 +9,6 @@ l1:
 	mov		byte [rdi], 0
 	inc		rdi
 	loop	l1
-=======
-	test rsi, rsi
-	je ret1
-	mov rcx, rsi
-
-l1:
-	mov byte [rdi], 0
-	inc rdi
-	loop l1
->>>>>>> b59b40bcdf8bd4c3d9568150ff7fb6c6be6251fe
 
 ret1:
 	ret
