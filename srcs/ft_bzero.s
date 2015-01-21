b59b40bcdf8bd4c3d9568150ff7fb6c6be6251fe
Kenny POIRIER <kpoirier@e2r10p11.42.fr>
2015-01-21 12:51:26 +0100

    too fast

--- srcs/ft_bzero.s
+++ srcs/ft_bzero.s
@@ -1,13 +1,15 @@
 section .text
 global _ft_bzero
 
-;rdi -> void * ptr
-;rsi -> size_t size
-
 _ft_bzero:
+	test rsi, rsi
+	je ret1
 	mov rcx, rsi
+
 l1:
 	mov byte [rdi], 0
 	inc rdi
 	loop l1
+
+ret1:
 	ret
