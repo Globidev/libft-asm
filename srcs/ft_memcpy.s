fdbcb72cd68697c350ca36a07766ea727d721801
Benoit CHEVALIER <bchevali@e2r11p11.42.fr>
2015-01-22 19:30:40 +0100

    Comments betonic on memcpy

--- srcs/ft_memcpy.s
+++ srcs/ft_memcpy.s
@@ -8,14 +8,14 @@ global _ft_memcpy
 ; rdx -> size_t n
 
 _ft_memcpy:
-    mov     rax,    rdi
-    cmp     rcx,    0
+    mov     rax,    rdi ;set return value
+    cmp     rdx,    0	;check nbr of rep 
     je     ret1
-    mov     rcx,    rdx
+    mov     rcx,    rdx ;set rcx
 
 copy:
     cld
-    rep     movsb
+    rep     movsb		;betonic memcpy
 
 ret1:
     ret
