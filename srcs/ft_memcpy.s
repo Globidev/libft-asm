b43278aa3505e9c25cae6e5c2cf2d795429d109d
Benoit CHEVALIER <bchevali@e2r11p11.42.fr>
2015-01-22 19:22:03 +0100

    memcpy Betonic

--- srcs/ft_memcpy.s
+++ srcs/ft_memcpy.s
@@ -1,18 +1,21 @@
 section .text
 global _ft_memcpy
 
+; rcx -> nbr de rep
+; rsi -> src
+; rdi -> dest
+; rep movsb
+; rdx -> size_t n
+
 _ft_memcpy:
     mov     rax,    rdi
-    test    rdx,    rdx
-    je      ret1
+    cmp     rcx,    0
+    je     ret1
     mov     rcx,    rdx
 
 copy:
-    mov     r15,    [rsi + rcx]
-    mov     [rdi + rcx],    r15b
-    loop    copy
-    mov     r15,    [rsi]
-    mov     [rdi],  r15b
+    cld
+    rep     movsb
 
 ret1:
     ret
