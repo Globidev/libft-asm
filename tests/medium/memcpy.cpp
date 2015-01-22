86bfacd02632f43adea00b74b3bfe10caeb85024
Benoit CHEVALIER <bchevali@e2r11p11.42.fr>
2015-01-22 19:50:38 +0100

    memcpy test beton

--- tests/medium/memcpy.cpp
+++ tests/medium/memcpy.cpp
@@ -8,17 +8,17 @@ extern "C" {
 
 void test_memcpy()
 {
-    // char    *systemCall;
+    char    *systemCall;
     char    *betonicCall;
     size_t  size = 11;
     char    str_sys[] = "Hello World";
     char    str_bet[] = "lolilolilol";
 
-    // systemCall = (char*)memcpy(str_sys, str_bet, size);
+    systemCall = (char*)memcpy(str_sys, str_bet, size);
     betonicCall = (char*)ft_memcpy(str_sys, str_bet, size);
 
-    // std::cout << systemCall << std::endl;
+    std::cout << systemCall << std::endl;
     std::cout << betonicCall << std::endl;
 
-    // assert(!memcmp(systemCall, betonicCall, size));
+    assert(!memcmp(systemCall, betonicCall, size));
 }
