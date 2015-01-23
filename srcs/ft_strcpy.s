c37d1b5d69afe65d6c3fa3673bbec815c55f9223
Oscar CHASE <ochase@student.42.fr>
2015-01-23 12:02:33 +0100

    just added ft_strcpy fix 'cause ochase can't seem to do it himself

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -4,16 +4,17 @@ global _ft_strcpy
 extern _ft_strlen
 
 _ft_strcpy:
-    mov     r10,    rdi
+    push    r12
+    mov     r12,    rdi
     mov     rdi,    rsi
     call    _ft_strlen
     cmp     rax,    0
-    je      ret_test
+    je      return
     mov     rcx,    rax
-    mov     rdi,    r10
+    mov     rdi,    r12
     mov     rax,    rdi
     rep     movsb
-    ret
+    pop     r12
 
-ret_test:
+return:
     ret
