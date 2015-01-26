a9c1223b3e44452603fa3c09319bd2879b01adb4
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-01-26 15:26:31 +0100

    Cleaned up memdel

--- srcs/ft_memdel.s
+++ srcs/ft_memdel.s
@@ -3,23 +3,20 @@ global _ft_memdel
 
 extern _free
 
+; void (void ** mem_ptr)
+;                  |
+;                  v
+;                 rdi
+
 _ft_memdel:
-    cmp     rdi,    0       ;check if rdi is null
-    je      ret_null
-    push    rdi             ;save base pointer
-    mov     rdi,    [rdi]   ;dereference pointer for _free
-
-dealloc:
+    cmp     rdi,    0           ; if (mem_ptr)
+    je      end
+    push    rbx                 ; using rbx to save mem_ptr (non volatile)
+    mov     rbx,    rdi
+    mov     rdi,    [rbx]       ; passing *mem_ptr to _free
     call    _free
-    pop     rdi
-    push    r12
-    xor     r12,      r12
-    mov     [rdi],    r12
-    pop     r12
+    mov     qword [rbx],    0   ; *mem_ptr = 0
+    pop     rbx
 
-
-ret_null:
-    mov     rax,    0
-
-return:
+end:
     ret
