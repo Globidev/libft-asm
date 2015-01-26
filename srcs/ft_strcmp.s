3352cd8f30563cc787e21ba37247b1cdbb70d019
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-01-26 16:52:29 +0100

    Cleaned up strcmp

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -1,44 +1,46 @@
 section .text
 global _ft_strcmp
 
+; int (const char * s1, const char * s2)
+;                    |                |
+;                    v                v
+;                   rdi              rsi
+
 _ft_strcmp:
     mov     rax,    0
-    cmp     rdi,    0
-    je      return
-    cmp     rsi,    0
-    je      return
+    mov     r8,     rdi
+    or      r8,     rsi ; if (!s1 && !s2)
+    cmp     r8,     0   ; return 0
+    je      equal
 
 cmp_loop:
     mov     r8,     [rdi]
-    mov     r9,     [rsi]
     cmp     r8,     0
     je      bcmp
-    cmp     r9,     0
+    cmp     byte [rsi],  0
     je      bcmp
-    cmp     r8b,    r9b
+    cmp     r8b,    byte [rsi]
     ja      greater
     jb      lower
     inc     rdi
     inc     rsi
     jmp     cmp_loop
 
-equal:
-    mov     rax,    0
-    jmp     return
-
-greater:
-    mov     rax,    1
-    jmp     return
-
-lower:
-    mov     rax,    -1
-    jmp     return
-
 bcmp:
-    cmp     r8b,    r9b
+    cmp     r8b,    byte [rsi]
     ja      greater
     jb      lower
     je      equal
 
-return:
+equal:
+    mov     rax,    0
     ret
+
+greater:
+    mov     rax,    1
+    ret
+
+lower:
+    mov     rax,    -1
+    ret
+
