7f412a8926117725cd2fb10782e6ecbdd3c23008
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-01-26 15:18:25 +0100

    Refactored memccpy + added more tests

--- tests/bonus/memccpy.cpp
+++ tests/bonus/memccpy.cpp
@@ -9,8 +9,8 @@ extern "C" {
 template <size_t n, class T, size_t src_n>
 static bool test_one(const T (&src)[src_n], const T * to_cpy, int c)
 {
-    T buff1[n];
-    T buff2[n];
+    T buff1[src_n];
+    T buff2[src_n];
 
     std::memcpy(buff1, src, src_n * sizeof(T));
     std::memcpy(buff2, src, src_n * sizeof(T));
@@ -18,11 +18,16 @@ static bool test_one(const T (&src)[src_n], const T * to_cpy, int c)
     auto ret = ::memccpy(buff1, to_cpy, c, n * sizeof(T));
     auto ft_ret = ::ft_memccpy(buff2, to_cpy, c, n * sizeof(T));
 
-    return !memcmp(buff1, buff2, n * sizeof(T)) &&
-            ((char *)ret - buff1) == ((char *)ft_ret - buff2);
+    bool mem_ok = !memcmp(buff1, buff2, src_n * sizeof(T));
+    return mem_ok && ((!ret && !ft_ret) ||
+                      ((char *)ret - buff1) == ((char *)ft_ret - buff2));
 }
 
 void test_memccpy_t::run()
 {
     assert(test_one<11>("Hello World", "lolilolilol", 'o'), "\"Hello World\" \"lolilolilol\" 'o' 11");
+    assert(test_one<2>("Hello World", "lolilolilol", 'o'), "\"Hello World\" \"lolilolilol\" 'o' 2");
+    assert(test_one<0>("Hello World", "lolilolilol", 'o'), "\"Hello World\" \"lolilolilol\" 'o' 0");
+    assert(test_one<42>("", "", 0), "empty empty 0 42");
+    assert(::memccpy(0, 0, 0, 0) == ::ft_memccpy(0, 0, 0, 0), "0 0 0 0");
 }
