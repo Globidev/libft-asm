199049b74c1c10ea388224257deb51c8dbd62e0f
Guillaume DEPARDON <guillaume.depardon@student.42.fr>
2015-02-05 18:39:07 +0100

    Better tests for list operations

--- tests/bonus/lstadd.cpp
+++ tests/bonus/lstadd.cpp
@@ -17,31 +17,25 @@ extern "C" {
 template <class T, size_t src_n>
 static bool test_one(const T (&src)[src_n])
 {
-    t_list  *betonic_lst;
+    t_list  *betonic_lst = nullptr, *new_elem, *new_elem2;
     bool    result;
 
-    betonic_lst = ft_lstnew("Base Elem", 10);
-    ft_lstadd(&betonic_lst, ft_lstnew(src, src_n));
-    result = !strcmp((char*)betonic_lst->content, src);
-    free(betonic_lst);
-    return result;
-}
-
-template <class T, size_t src_n>
-static bool test_two(const T (&src)[src_n])
-{
-    t_list  *betonic_lst;
-    bool    result;
-
-    betonic_lst = ft_lstnew("Base Elem", 10);
-    ft_lstadd(&betonic_lst, ft_lstnew(src, src_n));
-    result = (betonic_lst->content_size == src_n);
-    free(betonic_lst);
+    result = false;
+    new_elem = ft_lstnew("Base Elem", 10);
+    new_elem2 = ft_lstnew(src, src_n);
+    ft_lstadd(&betonic_lst, new_elem);
+    ft_lstadd(&betonic_lst, new_elem2);
+    result |= (new_elem == betonic_lst);
+    result |= (betonic_lst->next != 0);
+    result |= !strcmp(static_cast<char *>(betonic_lst->next->content), "Base Elem");
+    free(new_elem->content);
+    free(new_elem);
+    free(new_elem2->content);
+    free(new_elem2);
     return result;
 }
 
 void test_lstadd_t::run()
 {
-    assert(test_one("Content Test"), "Element: 'Content Test'");
-    assert(test_two("Size Testdddd"), "Element: 'Size Test'");
+    assert(test_one("Content Test"), "Create two elements");
 }
--- tests/bonus/lstnew.cpp
+++ tests/bonus/lstnew.cpp
@@ -19,39 +19,33 @@ static bool test_one(const T (&src)[src_n])
     t_list  *betonic_lst;
     bool    result;
 
+    result = true;
     betonic_lst = ft_lstnew(src, src_n);
-    result = !strcmp((char*)betonic_lst->content, src);
+    result |= !strcmp((char*)betonic_lst->content, src);
+    result |= (betonic_lst->content_size == src_n);
+    result |= (betonic_lst->next == 0);
+    free(betonic_lst->content);
     free(betonic_lst);
     return result;
 }
 
-template <class T, size_t src_n>
-static bool test_two(const T (&src)[src_n])
+static bool test_null()
 {
     t_list  *betonic_lst;
-    bool    result;
+    bool result;
 
-    betonic_lst = ft_lstnew(src, src_n);
-    result = (betonic_lst->content_size == src_n);
-    free(betonic_lst);
-    return result;
-}
-
-template <class T, size_t src_n>
-static bool test_three(const T (&src)[src_n])
-{
-    t_list  *betonic_lst;
-    bool    result;
-
-    betonic_lst = ft_lstnew(src, src_n);
-    result = (betonic_lst->next == 0);
+    result = true;
+    betonic_lst = ft_lstnew(0, 0);
+    result |= !betonic_lst->content;
+    result |= !betonic_lst->content_size;
+    result |= !betonic_lst->next;
+    free(betonic_lst->content);
     free(betonic_lst);
     return result;
 }
 
 void test_lstnew_t::run()
 {
-    assert(test_one("Content Test"), "Element: 'Content Test'");
-    assert(test_two("Size Test"), "Element: 'Size Test'");
-    assert(test_three("Next Test"), "Element: 'Next Test'");
+    assert(test_one("Hello world!\n"), "Create an element with a string");
+    assert(test_null(), "Create an element with null content");
 }
